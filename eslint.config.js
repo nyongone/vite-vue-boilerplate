5047ba79178277109e6b716ca2f312bb28bf3f80
조용원 <auau3@kakao.com>
2025-06-03 14:33:59 +0900

    repository initialization

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js, eslintPluginPrettier },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  eslintConfigPrettier,
+]);
---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex h-screen w-full items-center justify-center">
+    Hello World!
+  </div>
+</template>
