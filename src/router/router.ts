acadb1bf9fe3655232dd9524bf9e7f80d47c00a4
조용원 <auau3@kakao.com>
2025-06-12 15:48:52 +0900

    fix: router type 추가

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,6 +2,7 @@ import HomePage from "@/pages/HomePage.vue";
 import {
   createRouter,
   createWebHistory,
+  type Router,
   type RouteRecordRaw,
 } from "vue-router";
 
@@ -13,7 +14,7 @@ const routes: RouteRecordRaw[] = [
   },
 ];
 
-export const router = createRouter({
+export const router: Router = createRouter({
   history: createWebHistory(),
   routes: routes,
 });
