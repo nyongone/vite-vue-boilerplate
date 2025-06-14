e083220d90104beba1c2c7f17f1206a71cda59e1
조용원 <auau3@kakao.com>
2025-06-14 12:14:42 +0900

    fix: TestCounter 컴포넌트 삭제 및 관련 store 삭제

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="flex h-screen w-full flex-col items-center justify-center gap-4">
     <h1>Hello World!</h1>
-    <TestCounter />
   </div>
 </template>
-
-<script setup lang="ts">
-import TestCounter from "@/components/TestCounter.vue";
-</script>
