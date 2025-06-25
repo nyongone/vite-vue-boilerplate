6d0b3cf381a033ee98b1ea9d2423976299f31893
조용원 <auau3@kakao.com>
2025-06-25 12:26:24 +0900

    chore: 번들 분석 및 시각화 도구 (rollup-plugin-visualizer) 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    visualizer({
+      open: true,
+    }),
+  ],
 });
